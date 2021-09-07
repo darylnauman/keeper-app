61402c7d559897f6df254d6641a9d73726f619a1
Daryl Nauman <darylnauman@gmail.com>
2021-09-07 15:58:21 -0400

    completed part 1 of course challenge - added components, connected to stylesheet provided

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import App from './App'
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
 //1. Create a new React app. - DONE
 //2. Create a App.jsx component. - DONE
 //3. Create a Header.jsx component that renders a <header> element
-//to show the Keeper App name in an <h1>.
+//to show the Keeper App name in an <h1>. - DONE
 //4. Create a Footer.jsx component that renders a <footer> element
-//to show a copyright message in a <p> with a dynamically updated year.
+//to show a copyright message in a <p> with a dynamically updated year. - DONE
 //5. Create a Note.jsx component to show a <div> element with a
-//<h1> for a title and a <p> for the content.
+//<h1> for a title and a <p> for the content. - DONE
 //6. Make sure that the final website is styled like the example shown here:
-//https://l1pp6.csb.app/
+//https://l1pp6.csb.app/ - DONE
 
 //HINT: You will need to study the classes in the styles.css file to appy styling.
