6a5385c85ece97d508a3e3fd43ea7ae48a124450
Daryl Nauman <darylnauman@gmail.com>
2021-09-08 14:09:21 -0400

    completed part 2 of course challenge - using map and passing props to render multiple cards

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
+import notes from "./notes";
 
 const App = () => {
-  return(
-    <div>
-      <Header />
-      <Note />
-      <Footer />
-    </div>
-  );
-}
+    return (
+        <div>
+            <Header />
+
+            {notes.map((note) => (
+                <Note
+                    key={note.key}
+                    title={note.title}
+                    content={note.content}
+                />
+            ))}
+
+            <Footer />
+        </div>
+    );
+};
 
 export default App;
